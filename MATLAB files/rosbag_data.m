00252c020e880b8826b94724243451acae78f801
diogocostaf <diogocostaf@gmail.com>
2022-05-26 01:09:48 +0100

    Retrieves camera info from rosbag - image & video

--- MATLAB files/rosbag_data.m
+++ MATLAB files/rosbag_data.m
@@ -12,16 +12,24 @@ end
 if ~exist('./imagens/trajectory', 'dir')
     mkdir('imagens/trajectory');
 end
+if ~exist('./imagens/camera2', 'dir')
+    mkdir('imagens/camera2');
+end
+if ~exist('./video', 'dir')
+    mkdir('video');
+end
 
 %% Import data
-bag = rosbag('full_info.bag');
+rosbag_name = ('./rosbags/arucu_rosbag.bag');
+
+bag = rosbag(rosbag_name);
 start = bag.StartTime;
 finish = bag.EndTime;
 
 bagselect1 = select(bag,"Topic","/odom_gt");
 
 %% Print topics in screen
-rosbag info 'full_info.bag';
+rosbag info './rosbags/arucu_rosbag.bag';
 
 %% Slam Map Builder
 %slamMapBuilder
@@ -34,8 +42,8 @@ msgStructs = readMessages(bSel,'DataFormat','struct');
 xPoints = cellfun(@(m) double(m.Pose.Pose.Position.X),msgStructs);
 yPoints = cellfun(@(m) double(m.Pose.Pose.Position.Y),msgStructs);
 
-xPoints = xPoints(1:2279);
-yPoints = yPoints(1:2279);
+xPoints = xPoints(1:end);
+yPoints = yPoints(1:end);
 
 figure()
 plot(xPoints,yPoints)
@@ -44,39 +52,59 @@ ylabel("Y")
 title("Trajectory of the robot")
 saveas(gcf,"./imagens/trajectory/trajectory.png");
 
+
 %% Show image of the frontal camera
 
 startT = bag.StartTime;
 endT = bag.EndTime;
-depthLim =1000; %remove all depth data greater than depthLim
-fn = ('full_info.bag');
+%depthLim =1000; %remove all depth data greater than depthLim
 dataType = {'/head_camera/rgb/image_raw';'/head_camera/depth/image_raw';'/neck_camera/depth/image_raw'};    
 
-for i=1:2:3
+for i=1
     dataT=dataType{i};
-    bag = rosbag(fn);
     bagselect1 = select(bag, 'Topic', dataT);
     bagselect2 = select(bag, 'Time', [startT  endT], 'Topic', dataT);
     %bagselect2.AvailableTopics{1,end};
     bagselect3 = select(bagselect2, 'Time', [startT  endT]);
     msgs = readMessages(bagselect3);
-    msgs{1}
-    for j=1:5%size(msgs,1)
-        [img,alpha] = readImage(msgs{1});
+    msgs{1};
+    for j=1:size(msgs,1)
+        [img,alpha] = readImage(msgs{j});
         try
             figure(1)
             imshow(img,[min(min(img)) max(max(img))/10]);colormap(jet)
             img=double(img);
-            img(img>depthLim)=0; 
+            %img(img>depthLim)=0; 
             img(img==0)=NaN; % get rid of spurious data from mesh plot
-            figure(); mesh(img)
+            figure(2); mesh(img)
         catch
-            figure()
-            imshow(img)
+            figure(3);
+            imshow(img);
+            saveas(gcf,"./imagens/camera2/image_"+j+".png");
         end
         clear img
     end
 end
+close all
+
+
+%% Make a video of the robots' prespective
+
+imageNames = dir(fullfile("imagens/camera2",'image_','*.png'));
+imageNames = {imageNames.name}';
+outputVideo = VideoWriter(fullfile("video",'shuttle_out.avi'));
+
+%Choose the framerate of the video
+outputVideo.FrameRate = 10; 
+
+open(outputVideo)
+
+for ii = 1:size(msgs,1)
+   img = imread(fullfile("imagens/camera2","image_"+ii+".png"));
+   writeVideo(outputVideo,img)
+end
+
+close(outputVideo)
 
 %% Plot velocity over time
 bagselect = select(bag,"Time",[start finish]);
